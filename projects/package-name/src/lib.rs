c074d3a5a8b3caa2324231dc9cab4fa0f1d13b0d
Aster <galaster@foxmail.com>
2023-03-12 18:46:01 +0800

    📖 Define the package name

--- projects/package-name/src/lib.rs
+++ projects/package-name/src/lib.rs
@@ -1,3 +1,41 @@
-mod errors;
+use std::fmt::{Debug, Display, Formatter};
 
-pub use errors::{Error, Result};
+pub struct UniqueKey {
+    key: String,
+}
+
+impl Debug for UniqueKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(&self.key, f)
+    }
+}
+
+impl Display for UniqueKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self.key, f)
+    }
+}
+
+impl UniqueKey {
+    pub fn new<S>(key: S) -> Self
+        where
+            S: Into<String>,
+    {
+        Self { key: key.as_ref().to_string() }
+    }
+    pub fn as_str(&self) -> &str {
+        &self.key
+    }
+}
+
+impl Default for UniqueKey {
+    fn default() -> Self {
+        Self::new("")
+    }
+}
+
+impl AsRef<str> for UniqueKey {
+    fn as_ref(&self) -> &str {
+        &self.key
+    }
+}
